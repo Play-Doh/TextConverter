5599e3fe74b0f2b1fa11728a11c28b05854b217c
Mark McCall <mark.mccall@outlook.com>
2015-10-05 18:41:25 -0400

    First commit of TextConverter

---
+++ TextConverter/Page.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextConverter
+{
+    class Page
+    {
+        private List<StringBuilder> lines { get; set; }
+        private string size { get; set; }
+
+        public List<StringBuilder> Lines
+        {
+            get { return lines; }
+            set { lines = value; }
+        }
+
+        public string Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+
+        public Page()
+        { }
+
+        public Page(List<StringBuilder> Lines, string Size = "LETTER")
+        {
+            this.lines = Lines;
+            this.size = Size;
+        }
+    }
+}
---
+++ TextConverter/PrintFIle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextConverter
+{
+    class PrintFile
+    {
+        private List<Page> pages { get; set; }
+        private string frontunderlay { get; set; }
+        private string backunderlay { get; set; }
+        private string plex { get; set; }
+
+        public List<Page> Pages
+        {
+            get { return pages; }
+            set { pages = value; }
+        }
+
+        public string FrontUnderlay
+        {
+            get { return frontunderlay; }
+            set { frontunderlay = value; }
+        }
+
+        public string BackUnderlay
+        {
+            get { return backunderlay; }
+            set { backunderlay = value; }
+        }
+
+        public string Plex
+        {
+            get { return plex; }
+            set { plex = value; }
+        }
+
+        public PrintFile()
+        { }
+
+        public PrintFile(List<Page> Pages, string Plex = "SIMPLEX", string FrontUnderlay = "", string BackUnderlay = "")
+        {
+            this.pages = Pages;
+            this.plex = Plex;
+            this.frontunderlay = FrontUnderlay;
+            this.backunderlay = BackUnderlay;
+        }
+    }
+}
